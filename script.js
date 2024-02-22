fe5bc1af7f7667f7f41ea90a67dce0e181a4d414
Vayer88 <95075451+Vayer88@users.noreply.github.com>
2024-02-22 04:12:34 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let insideOfCave = document.getElementById('inside');
+let outlineOfCave = document.getElementById('outline');
+
+insideOfCave.addEventListener("click", () => {
+    document.getElementById('textbox').innerText = "Here is the entrance to the cave";
+    })
